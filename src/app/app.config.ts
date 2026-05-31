68771a65335cf555ae3d88a7ee99b747fb54f1e6
CesarGomez25 <cc4cesar@hotmail.com>
2026-05-31 11:36:59 -0600

    update nom y angular version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { DataService } from './services/data.service';
-import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
-
-const services = [
-  DataService
-]
 
 export const appConfig: ApplicationConfig = {
-  //providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
-  providers: [services, {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}},
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay())]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), provideClientHydration(withEventReplay())
+  ]
 };
