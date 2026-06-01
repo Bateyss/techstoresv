09c7ec6bdab7c1f1ddf7951afca796600de71d47
CesarGomez25 <cc4cesar@hotmail.com>
2026-05-31 18:24:46 -0600

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './components/inicio/inicio';
+import { Productos } from './components/productos/productos';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/menu', pathMatch: 'full' },
+    {
+        path: 'menu', component: Inicio, children: [
+            { path: '', redirectTo: '/menu/login', pathMatch: 'full' },
+            { path: 'login', component: Login, },
+            { path: 'productos', component: Productos, },
+        ]
+    }
+];
