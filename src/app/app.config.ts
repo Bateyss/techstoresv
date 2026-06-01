313b952a9cde30c034808d27941f52f0011e7410
CesarGomez25 <cc4cesar@hotmail.com>
2026-05-31 18:20:46 -0600

    update configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './app.routes';
+import { DataService } from './services/data.service';
+
+const services = [
+  DataService
+]
 
 export const appConfig: ApplicationConfig = {
+  // providers: [
+  //   provideBrowserGlobalErrorListeners(),
+  //   provideRouter(routes), provideClientHydration(withEventReplay())
+  // ]
   providers: [
+    services,
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(withEventReplay())
   ]
 };
