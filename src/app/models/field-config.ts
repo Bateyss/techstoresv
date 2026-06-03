0d08cda38dc7a6408a62969dd93b51034cf4da1b
CesarGomez25 <cc4cesar@hotmail.com>
2026-06-03 07:29:40 -0600

    add form service models

---
+++ src/app/models/field-config.ts
@@ -0,0 +1,12 @@
+import { SelectOption } from "./select-option";
+
+export interface FieldConfig {
+    id: number,
+    name: string,
+    label: string,
+    value?: any,
+    type: 'text' | 'email' | 'password' | 'number' ,
+    controlType: 'input' | 'select' | 'textarea',
+    options?: Array<SelectOption>,
+    validators?: any[]
+}
