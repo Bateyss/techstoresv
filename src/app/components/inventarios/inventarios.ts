9f70b4e24f8098bd58f4971ce76c8bbc1fabb6c6
CesarGomez25 <cc4cesar@hotmail.com>
2026-06-11 00:35:21 -0600

    update inventarios

--- src/app/components/inventarios/inventarios.ts
+++ src/app/components/inventarios/inventarios.ts
@@ -1,16 +1,15 @@
 import { Component, DOCUMENT, Inject, inject, OnInit, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Router } from '@angular/router';
+import { MaterialTableDialogModule, MaterialTableModelModule } from '../../material/material.module';
+import { MovimientoInventario } from '../../models/movimiento-inventario';
 import { Producto } from '../../models/producto';
+import { TipoMovimiento } from '../../models/tipo-movimiento';
 import { DataService } from '../../services/data.service';
 import { InventarioService } from '../../services/inventario-service';
-import { Utils } from '../../util/utils';
-import { MaterialTableDialogModule, MaterialTableModelModule } from '../../material/material.module';
-import { MatDividerModule } from '@angular/material/divider';
 import { DataApp } from '../../util/data-app';
-import { MovimientoInventario } from '../../models/movimiento-inventario';
+import { Utils } from '../../util/utils';
 
 @Component({
   selector: 'app-inventarios',
@@ -20,15 +19,14 @@ import { MovimientoInventario } from '../../models/movimiento-inventario';
 })
 export class Inventarios implements OnInit {
 
-  private _snackBar = inject(MatSnackBar);
   public innerWidths = '0';
   private document = inject(DOCUMENT);
   private dialog = inject(MatDialog);
 
-  private inventarioService = inject(InventarioService);
   private dataService = inject(DataService);
 
   public productosList = signal<Array<Producto>>([]);
+  public tipoMovimientoList = signal<Array<TipoMovimiento>>([]);
 
   constructor(private _router: Router,) { }
 
@@ -39,20 +37,41 @@ export class Inventarios implements OnInit {
 
   cargarListas() {
     this.productosList.update(valores => this.dataService.getProductos());
+    this.tipoMovimientoList.update(valores => this.dataService.getTiposMovimiento());
+  }
+
+  verCompras() {
+    this.verMovimientosTipo(this.tipoMovimientoList()[this.tipoMovimientoList().findIndex(a => a.id == 1)]);
+  }
+
+  verVentas() {
+    this.verMovimientosTipo(this.tipoMovimientoList()[this.tipoMovimientoList().findIndex(a => a.id == 2)]);
   }
 
   verMovimientosProducto(producto: Producto) {
-
     const dialogConfig = Utils.getMatDialogConf()
     dialogConfig.data = {
       producto: producto
     };
+    this.dialog.open(MovimientosProductoDialog, dialogConfig);
+  }
 
-    const dialogRef = this.dialog.open(MovimientosProductoDialog, dialogConfig);
-    dialogRef.afterClosed().subscribe(
-      result => {
-        this.cargarListas();
-      });
+  verMovimientosTipo(tipoMovimiento: TipoMovimiento) {
+    const dialogConfig = Utils.getMatDialogConf()
+    dialogConfig.data = {
+      tipoMovimiento: tipoMovimiento
+    };
+    this.dialog.open(MovimientosTipoDialog, dialogConfig);
+  }
+
+  verMovimientosDiario() {
+    const dialogConfig = Utils.getMatDialogConf()
+    this.dialog.open(MovimientosLibroDiarioDialog, dialogConfig);
+  }
+
+  verMovimientosMayor() {
+    const dialogConfig = Utils.getMatDialogConf()
+    this.dialog.open(MovimientosLibroMayorDialog, dialogConfig);
   }
 
 }
@@ -75,7 +94,6 @@ export class MovimientosProductoDialog implements OnInit {
   private producto = signal<Producto>(DataApp.productoVacio());
 
   public movimientosList = signal<Array<MovimientoInventario>>([]);
-  public pedido = DataApp.pedidoVacio();
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {
     producto: Producto
@@ -90,6 +108,130 @@ export class MovimientosProductoDialog implements OnInit {
 
   cargarListas() {
     this.movimientosList.update(valores => [...this.inventarioService.getMovimientosProducto(this.producto())]);
+  }
+
+  cerrarDetalles() {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'dialog-detalles-tipo',
+  imports: [MaterialTableDialogModule],
+  templateUrl: './movimientos.tipo.html',
+  styleUrl: './inventarios.css'
+})
+export class MovimientosTipoDialog implements OnInit {
+
+  public innerWidths = '0';
+  private document = inject(DOCUMENT);
+
+  readonly dialogRef = inject(MatDialogRef<MovimientosTipoDialog>);
+
+  private inventarioService = inject(InventarioService);
+
+  public tipoMovimiento = signal<TipoMovimiento>(DataApp.tipoMovimientoVacio());
+
+  public movimientosList = signal<Array<MovimientoInventario>>([]);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {
+    tipoMovimiento: TipoMovimiento
+  }) {
+    this.innerWidths = (this.document.body.clientWidth * 0.9) + 'px';
+    this.tipoMovimiento.update(valor => data.tipoMovimiento);
+  }
+
+  ngOnInit() {
+    this.cargarListas();
+  }
+
+  cargarListas() {
+    this.movimientosList.update(valores => [...this.inventarioService.getMovimientosTipoMovimiento(this.tipoMovimiento())]);
+  }
+
+  cerrarDetalles() {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'dialog-detalles-tipo',
+  imports: [MaterialTableDialogModule],
+  templateUrl: './movimientos.diario.html',
+  styleUrl: './inventarios.css'
+})
+export class MovimientosLibroDiarioDialog implements OnInit {
+
+  public innerWidths = '0';
+  private document = inject(DOCUMENT);
+
+  readonly dialogRef = inject(MatDialogRef<MovimientosLibroDiarioDialog>);
+
+  private dataService = inject(DataService);
+
+  public movimientosList = signal<Array<MovimientoInventario>>([]);
+
+  constructor() {
+    this.innerWidths = (this.document.body.clientWidth * 0.9) + 'px';
+  }
+
+  ngOnInit() {
+    this.cargarListas();
+  }
+
+  cargarListas() {
+    this.movimientosList.update(valores => [...this.dataService.getMovimientosInventario()]);
+  }
+
+  cerrarDetalles() {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'dialog-detalles-tipo',
+  imports: [MaterialTableDialogModule],
+  templateUrl: './movimientos.mayor.html',
+  styleUrl: './inventarios.css'
+})
+export class MovimientosLibroMayorDialog implements OnInit {
+
+  public innerWidths = '0';
+  private document = inject(DOCUMENT);
+
+  readonly dialogRef = inject(MatDialogRef<MovimientosLibroMayorDialog>);
+
+  private dataService = inject(DataService);
+
+  public movimientosList = signal<Array<MovimientoInventario>>([]);
+  public tipoMovimientoList = signal<Array<TipoMovimiento>>([]);
+
+  constructor() {
+    this.innerWidths = (this.document.body.clientWidth * 0.9) + 'px';
+  }
+
+  ngOnInit() {
+    this.cargarListas();
+  }
+
+  cargarListas() {
+    this.movimientosList.update(valores => [...this.dataService.getMovimientosInventario()]);
+    this.tipoMovimientoList.update(valores => [...this.dataService.getTiposMovimiento()]);
+  }
+
+  movimientosPorTipo(tipo: TipoMovimiento): Array<MovimientoInventario> {
+    let movimientos = this.movimientosList();
+    let movimientosPorTipo: Array<MovimientoInventario> = [];
+
+    for (const movimiento of movimientos) {
+      if (movimiento.tipo_movimiento.id == tipo.id) {
+        movimientosPorTipo.push(movimiento);
+      }
+    }
+    return movimientosPorTipo;
   }
 
   cerrarDetalles() {
--- src/app/services/inventario-service.ts
+++ src/app/services/inventario-service.ts
@@ -3,6 +3,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { DataService } from './data.service';
 import { Producto } from '../models/producto';
 import { MovimientoInventario } from '../models/movimiento-inventario';
+import { TipoMovimiento } from '../models/tipo-movimiento';
 
 @Injectable({
   providedIn: 'root',
@@ -27,5 +28,16 @@ export class InventarioService {
     return movimmientosProducto;
   }
 
+  getMovimientosTipoMovimiento(tipoMovimiento: TipoMovimiento): Array<MovimientoInventario> {
+    let movimientos = this.dataService.getMovimientosInventario();
+    let movimmientosPorTipoMovimiento: Array<MovimientoInventario> = [];
 
+    for (const movimiento of movimientos) {
+      if (movimiento.tipo_movimiento.id == tipoMovimiento.id) {
+        movimmientosPorTipoMovimiento.push(movimiento);
+      }
+    }
+
+    return movimmientosPorTipoMovimiento;
+  }
 }
